34c18d0e4d046ae458ac0f2e4cf287b0ce667658
Gregory S. Turenchalk <gregory.turenchalk@roche.com>
2014-07-30 02:47:27 -0400

    Removing two tests that are now unnecessary.

--- nimblegen_heatseq/src/test/java/com/roche/heatseq/process/PrimerReadExtensionAndPcrDuplicateIdentificationTest.java
+++ nimblegen_heatseq/src/test/java/com/roche/heatseq/process/PrimerReadExtensionAndPcrDuplicateIdentificationTest.java
@@ -92,32 +92,4 @@ public class PrimerReadExtensionAndPcrDuplicateIdentificationTest {
 		assertEquals(mergeInformation.getMergedQuality(), "HHHHHHHHHH");
 	}
 
-	// The following two tests are temporary placeholders to illustrate a type of problem data that can currently slip through the code without failing tests.
-	@Test(groups = { "unit" })
-	public void alignmentNotShiftedOneTest() {
-		// This test paired with the alignmentShiftedOneTest and this is supposed to show a baseline proper alignment.
-		ISequence upstreamSequence = new IupacNucleotideCodeSequence("TGGTGAACC");
-		String upstreamQuality = "FGBF1@3FF";
-		ISequence downstreamSequence = new IupacNucleotideCodeSequence("GGTGAACCGCAT");
-		String downstreamQuality = "GF1GGGCGGFC@";
-		MergeInformation mergeInformation = PrimerReadExtensionAndPcrDuplicateIdentification.mergeSequences(false, upstreamSequence, downstreamSequence, upstreamQuality, downstreamQuality, 82, 83);
-		assertEquals(mergeInformation.getMergedSequence(), new IupacNucleotideCodeSequence("TGGTGAACCGCAT"));
-		assertEquals(mergeInformation.getMergedQuality(), "FGFFGGGFGGFC@");
-	}
-
-	@Test(groups = { "unit" })
-	public void alignmentShiftedOneTest() {
-		// This test shows how the pair from the alignmentNotShiftedOneTest can produce incorrect merged output when the alignment starts get flipped, swapping upstream and downstream in the process.
-		ISequence upstreamSequence = new IupacNucleotideCodeSequence("GGTGAACCGCAT");
-		String upstreamQuality = "GF1GGGCGGFC@";
-		ISequence downstreamSequence = new IupacNucleotideCodeSequence("TGGTGAACC");
-		String downstreamQuality = "FGBF1@3FF";
-
-		MergeInformation mergeInformation = PrimerReadExtensionAndPcrDuplicateIdentification.mergeSequences(false, upstreamSequence, downstreamSequence, upstreamQuality, downstreamQuality, 82, 83);
-		// Note that this merge is incorrect biologically, it should actually be "GGTGAACCGCAT"
-		assertEquals(mergeInformation.getMergedSequence(), new IupacNucleotideCodeSequence("GGGGAACCGCAT"));
-		assertEquals(mergeInformation.getMergedQuality(), "GFGGGGCGGFC@");
-		// This test is a reference that will pass, and can be removed once we are sure we have fixed the underlying problem.
-	}
-
 }
