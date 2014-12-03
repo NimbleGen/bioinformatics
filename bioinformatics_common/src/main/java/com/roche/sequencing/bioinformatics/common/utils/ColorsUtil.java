5ed73ba3a4335f666a4d6330265fcb321778810d
heilmank <kurt.heilman@roche.com>
2014-12-03 15:37:23 -0600

    changes to colorsutil

--- bioinformatics_common/src/main/java/com/roche/sequencing/bioinformatics/common/utils/ColorsUtil.java
+++ bioinformatics_common/src/main/java/com/roche/sequencing/bioinformatics/common/utils/ColorsUtil.java
@@ -4,47 +4,26 @@ import java.awt.Color;
 
 public class ColorsUtil {
 
-	/**
-	 * taken and modified from java.awt.Color
-	 * 
-	 * @param color
-	 * @param factor
-	 *            (0 through 1 inclusive) a factor of zero returns the same color and a factor of 1 will return the brightest possible color of the provided color
-	 * 
-	 * @return
-	 */
-	public static Color getBrighterColor(Color color, double factor) {
-		Color newColor = null;
-		if (factor < 0 || factor > 1) {
-			throw new IllegalStateException("The provided factor[" + factor + "] must be between 0 and 1 inclusive.");
+	public static Color getBrighterColor(Color color, double ratio) {
+		float[] hsb = Color.RGBtoHSB(color.getRed(), color.getGreen(), color.getBlue(), new float[3]);
+		double brightness = hsb[2];
+		double newBrightness = brightness;
+
+		Color newColor = color;
+		if (ratio != 0.5) {
+			if (ratio < 0.5) {
+				double darkerRatio = ratio * 2;
+				newBrightness = (darkerRatio * brightness);
+			} else if (ratio > 0.5) {
+				double brighterRatio = (ratio - 0.5) * 2;
+				newBrightness = brightness + (brighterRatio * (1 - brightness));
+			}
+			int newRGB = Color.HSBtoRGB(hsb[0], hsb[1], (float) newBrightness);
+			int red = (newRGB >> 16) & 0xFF;
+			int green = (newRGB >> 8) & 0xFF;
+			int blue = newRGB & 0xFF;
+			newColor = new Color(red, green, blue, color.getAlpha());
 		}
-		factor = 1 - factor;
-
-		int r = color.getRed();
-		int g = color.getGreen();
-		int b = color.getBlue();
-		int alpha = color.getAlpha();
-
-		/*
-		 * From 2D group: 1. black.brighter() should return grey 2. applying brighter to blue will always return blue, brighter 3. non pure color (non zero rgb) will eventually return white
-		 */
-		int i = (int) (1.0 / (1.0 - factor));
-		if (r == 0 && g == 0 && b == 0) {
-			newColor = new Color(i, i, i, alpha);
-		} else {
-			if (r > 0 && r < i) {
-				r = i;
-			}
-			if (g > 0 && g < i) {
-				g = i;
-			}
-			if (b > 0 && b < i) {
-				b = i;
-			}
-			newColor = new Color(Math.min((int) (r / factor), 255), Math.min((int) (g / factor), 255), Math.min((int) (b / factor), 255), alpha);
-		}
-
 		return newColor;
 	}
-
 }
