9636e8060b523230e342e385b14a4eb36fb42b64
heilmank <kurt.heilman@roche.com>
2015-11-13 09:36:14 -0600

    changing copyright date to 2016

--- nimblegen_heatseq/src/main/java/com/roche/heatseq/process/FilterByUid.java
+++ nimblegen_heatseq/src/main/java/com/roche/heatseq/process/FilterByUid.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2013 Roche NimbleGen Inc.
+ *    Copyright 2016 Roche NimbleGen Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
